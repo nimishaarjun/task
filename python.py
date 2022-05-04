83ee242876ab6e31e156f0b8a58514a9b0a99209
nimishaarjun <104893498+nimishaarjun@users.noreply.github.com>
2022-05-04 15:39:32 +0530

    multithreading

--- python.py
+++ python.py
@@ -1,1 +1,22 @@
 print("hello")
+from threading import *
+from time import sleep
+class hello(Thread):
+    def run(self):
+        for i in range(5):
+            print("hello")
+            sleep(1)
+class hi(Thread):
+    def run(self):
+        for i in range(5):
+            print("hy")
+            sleep(1)
+t1=hello()
+t2=hi()
+t1.start()   #have to use start instead of run,because run method is already used in Thread class
+sleep(.2)     #for avoiding collition
+t2.start()
+t1.join()
+t2.join()
+print("bye")
+
